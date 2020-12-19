3b10178b204c378d033117c31cff65656d05648b
ColdReader94 <54440204+ColdReader94@users.noreply.github.com>
2020-12-19 17:08:34 +0300

    Update script.js

--- pages/main/script.js
+++ pages/main/script.js
@@ -113,7 +113,8 @@ async function generateSlides() {
 
 // Генерация рандомного числа
 function randIntExcep(min, max, exp) {
-      var n, exp = Array.isArray(exp) ? exp : [(isNaN(exp) ? min-1 : exp)];
+    let n;
+    exp = Array.isArray(exp) ? exp : [(isNaN(exp) ? min-1 : exp)];
     while(true){
         n = Math.floor(Math.random() * (max - min + 1)) + min;
         if(exp.indexOf(n) < 0) return n;
